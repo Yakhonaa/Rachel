d3005546693d0b30c3b0cd71d1f0e5c2411d6484
max <karambit.max@gmail.com>
2025-09-27 15:37:51 +0300

    shit

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit on error
 
-# Install Node.js
-apt-get update && apt-get install -y curl gnupg
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
-apt-get install -y nodejs
-
 # Install Python deps
 pip install -r requirements.txt
+
+# Install Node.js inside the Python venv
+nodeenv -p --node=18.16.0
+
